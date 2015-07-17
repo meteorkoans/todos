d0f0681dbea1290551688963aac3872db770b6fc
Calvin Cheng <linchuan.cheng@gmail.com>
2015-07-17 23:28:33 +0800

    Showing todos count

--- todo.js
+++ todo.js
@@ -63,8 +63,16 @@ if (Meteor.isClient) {
     }
   });
 
-}
+  Template.todosCount.helpers({
+    'totalTodos': function() {
+      return Todos.find().count();
+    },
+    'completedTodos': function() {
+      return Todos.find({ completed: true }).count();
+    }
+  });
 
+}
 
 if (Meteor.isServer) {
 
